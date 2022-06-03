f1b4f49f4e34ed2d553a0c06edccd7b9fb4cff59
bjhobson12 <bjhobson12@gmail.com>
2022-06-03 16:17:30 -0400

    Works now

--- new.py
+++ new.py
@@ -5,17 +5,23 @@ import random
 import matplotlib.pyplot as plt
 import sys
 
-def sigmoid(x):
-  return 1 / (1 + exp(-x))
-
 class NN():
 
     def __init__(self, learning_rate=0.01):
         self.learning_rate = learning_rate
-        self.hidden_layer = np.random.rand(5,4+1)*2 - 1 # matrix
+        self.hidden_layer = np.random.rand(5,3+1)*2 - 1 # matrix
         self.output_layer = np.random.rand(1,5+1)*2 - 1 # matrix
         self.layers = [self.hidden_layer, self.output_layer]
         self.deltas = []
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_prime_y(self, y):
+        return y * (1 - y) 
+
+    def sigmoid_prime(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
 
     def forward(self, inp):
         assert type(inp) is np.ndarray
@@ -24,11 +30,10 @@ class NN():
 
         for layer_index in range(len(self.layers)):
             previous_input = np.append(inp, 1) if layer_index == 0 else np.append(self.outputs[layer_index - 1], 1)
-            layer_output = np.array([])
+            self.outputs[layer_index] = np.array([])
             for nueron in self.layers[layer_index]:
-                layer_output = np.append(layer_output, sigmoid(nueron.dot(previous_input)))
-
-            self.outputs[layer_index] = layer_output
+                d = self.sigmoid(nueron.dot(previous_input))
+                self.outputs[layer_index] = np.append(self.outputs[layer_index], d)
 
         return self.outputs[-1]
 
@@ -37,22 +42,17 @@ class NN():
         assert 1 == expected.ndim and 1 == inp.ndim
         self.deltas = [None]*len(self.layers)
 
-        for layer_index in reversed(range(len(self.layers))):
-            delta_output = np.array([])
-            for nueron_index in range(len(self.layers[layer_index])):
-                output = self.outputs[layer_index][nueron_index]
+        cost_total = np.power(expected - self.outputs[-1], 2).sum()*0.5
 
-                if layer_index == len(self.layers) - 1:
-                    delta = (output - expected[nueron_index]) * output * (1 - output)
-                else:
-                    error = self.layers[layer_index + 1][:, nueron_index].dot(self.deltas[layer_index + 1])
-                    delta = output * (1 - output) * error
-                    
-                delta_output = np.append(delta_output, delta)
-            
-            self.deltas[layer_index] = delta_output
+        self.deltas[-1] = cost_total * self.outputs[-1] * (1 - self.outputs[-1])  # derivative of sigmoid
+
+        for layer_index in reversed(range(len(self.layers) - 1)):
+            delta_list = self.layers[layer_index + 1][:,:-1].T @ self.deltas[layer_index + 1]
+            self.deltas[layer_index] = self.outputs[layer_index] * (1 - self.outputs[layer_index]) * delta_list
 
         self.__update(inp)
+
+        return np.average(np.abs(expected - self.outputs[-1]))
 
     def __update(self, inp):
         for layer_index in range(len(self.layers)):
@@ -60,9 +60,9 @@ class NN():
             for nueron_index in range(len(self.layers[layer_index])):
                 self.layers[layer_index][nueron_index] -= self.deltas[layer_index][nueron_index] * self.learning_rate * previous_input
 
-        
 
-model = NN(learning_rate=0.01)
+
+model = NN(learning_rate=0.1)
 
 """
 Even num 1a then output should be 0, else 1
@@ -71,32 +71,50 @@ Even num 1a then output should be 0, else 1
 dataset = [(0, 0, 0, 0), (0, 0, 0, 1), (0, 0, 1, 0), (0, 0, 1, 1), (0, 1, 0, 0), (0, 1, 0, 1), (0, 1, 1, 0), (0, 1, 1, 1),
        (1, 0, 0, 0), (1, 0, 0, 1), (1, 0, 1, 0), (1, 0, 1, 1), (1, 1, 0, 0), (1, 1, 0, 1), (1, 1, 1, 0), (1, 1, 1, 1)]
 
-epochs = 1000
+# input data
+inputs = np.array([[0, 1, 0],
+                   [0, 1, 1],
+                   [0, 0, 0],
+                   [1, 0, 0],
+                   [1, 1, 1],
+                   [1, 0, 1]])
+# output data
+outputs = np.array([[0], [0], [0], [1], [1], [1]])
+
+epochs = 25000
 
 errors = []
 
 for e in range(epochs):
     print("Epoch {}/{}".format(e + 1, epochs))
-    random.shuffle(dataset)
 
     error = 0
-    
-    for inp in dataset:
-        output = model.forward(np.asarray(inp))[0]
-        output = 1 if output >= 0.5 else 0
-        model.backprop(np.asarray(inp), np.array([inp.count(1) % 2 == 0]).astype(int))
 
-        if output != inp.count(1) % 2:
-          error += 1
+    random.shuffle(inputs)
+    
+    inp = inputs[0]
+    expected_output = outputs[0]
+    output = model.forward(inp)[0]
+    #print(output, 0 if output <= 0.5 else 1)
+    output = 0 if output <= 0.5 else 1
+    # output = 1 if output >= 0.5 else 0
+    cost_total = model.backprop(inp, expected_output)
         
     
-    errors.append(error/16.0)
-
+    errors.append(cost_total)
+    
 
 plt.plot(errors)
 plt.ylim(0,1)
-plt.savefig("./myfig.png")
-plt.close()
+plt.show()
+
+
+
+
+
+
+
+
 
 
 
